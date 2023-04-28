1fd9934c0a1be29177fbec62268502453c77f847
KihwanCheon <gujjy97@gmail.com>
2023-04-28 10:43:32 +0900

    Add Thread examples

---
+++ Examples/Thread01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread01
+{
+    class Program
+    {
+        static void MainThread()
+        {
+            while(true)
+            //for (int i = 0; i < 5; ++i)
+                Console.WriteLine("Hello Thread!");
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(MainThread);
+            t.Name = "Test Thread";
+            t.IsBackground = true; // 메인스레드 종료시 스레드도 종료.
+            t.Start();
+            
+            Console.WriteLine("Waiting for Thread");
+            t.Join();
+            Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ Examples/Thread02-ThreadPool/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread02_ThreadPool
+{
+    class Program
+    {
+        static void MainThreadForPool(object state)
+        {
+            var currTrd = Thread.CurrentThread;
+
+            for (int i = 0; i < 5; ++i)
+            {
+                Console.WriteLine($"Hello {state}. ThreadPool!" +
+                                  $" ThreadId: {currTrd.ManagedThreadId}" +
+                                  $", isThreadPoolThread: {currTrd.IsThreadPoolThread}" +
+                                  $", isBackground: {currTrd.IsBackground} " );
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            const int maxThreadCnt = 5;
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(maxThreadCnt, maxThreadCnt);
+
+            for (int i = 0; i < maxThreadCnt -1; ++i)        // 오래 걸리는 작업으로 스레드 풀을 점유한다.
+                ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+
+            ThreadPool.QueueUserWorkItem(MainThreadForPool, "1"); // IsBackground.
+            ThreadPool.QueueUserWorkItem(MainThreadForPool, "2"); // IsBackground.
+            ThreadPool.QueueUserWorkItem(MainThreadForPool, "3"); // IsBackground.
+            
+            Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
