6099d8e8b4efa990b02619226a867da799361341
KihwanCheon <gujjy97@gmail.com>
2023-05-04 10:10:09 +0900

    Add Optimization example - 01

---
+++ Examples/CompilerOptimization01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CompilerOptimization01
+{
+    internal class Program
+    {
+        private static bool _stop = false;
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("스레드 시작!");
+
+            while (!_stop)
+            {
+                // wait stop == false
+            }
+
+            Console.WriteLine("스레드 종료!");
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            var t = new Task(ThreadMain);
+
+            t.Start();
+
+            Thread.Sleep(1000);
+
+            _stop = true;
+
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+
+            t.Wait();
+            
+            Console.WriteLine("종료 성공!");
+        }
+    }
+}
