1ab3d76c72c4080e04ed6f18d1fa7bf41f3865cd
KihwanCheon <gujjy97@gmail.com>
2023-05-04 10:21:43 +0900

    [Optimization] volatile keyward - not recommended

--- Examples/CompilerOptimization01/Program.cs
+++ Examples/CompilerOptimization01/Program.cs
@@ -2,17 +2,27 @@
 using System.Threading;
 using System.Threading.Tasks;
 
+/*
+ * Release 모드로 실행하면서 비정상적인 동작 확인.
+ */
 namespace CompilerOptimization01
 {
     internal class Program
     {
-        private static bool _stop = false;
+        /*
+          volatile를 붙이면 최적화 하지 않아서 Release 모드에서도 정상동작하나 권장하지는 않음.
+          volatile 최적화 하지 말고 캐시무시 말고 최신값을 가져다 사용하라는 의미, C#에서 이상하게 동작하니(?) 사용하지 않기를 권함. 
+          volatile c++ 에도 있으나 의미가 다름. 
+        */
+        // volatile
+        static bool _stop = false;
+
 
         static void ThreadMain()
         {
             Console.WriteLine("스레드 시작!");
 
-            while (!_stop)
+            while (!_stop)          // Release 모드로 컴파일하면 최적화 됨.
             {
                 // wait stop == false
             }
