7134c1ce972a6382b98bccec583c776883c878f6
KihwanCheon <gujjy97@gmail.com>
2023-05-24 10:10:42 +0900

    Add SpinLock example - bug... exists

---
+++ Examples/MT10-SpinLock/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MT10_SpinLock
+{
+    class SpinLock
+    {
+        private volatile bool _locked = false;
+
+        public void Acquire()
+        {
+            while (_locked)
+            {
+            }
+
+            _locked = true;
+        }
+
+        public void Release()
+        {
+
+            _locked = false;
+        }
+    }
+
+
+    internal class Program
+    {
+        private static int _num = 0;
+        private static SpinLock _lock = new SpinLock();
+
+        private const int LoopCnt = 10000;
+
+
+        static void Thread_01()
+        {
+            for (int i = 0; i < LoopCnt; i++)
+            {
+                _lock.Acquire();
+                ++_num;
+                _lock.Release();
+            }
+        }
+        
+        static void Thread_02()
+        {
+            for (int i = 0; i < LoopCnt; i++)
+            {
+                _lock.Acquire();
+                --_num;
+                _lock.Release();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var t1 = new Task(Thread_01);
+            var t2 = new Task(Thread_02);
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+            
+            Console.WriteLine($"Hello World! {_num}");
+        }
+    }
+}
