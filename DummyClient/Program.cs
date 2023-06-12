523fe8d3830e631c98a457ec9a83309ff0c3073c
KihwanCheon <gujjy97@gmail.com>
2023-06-12 10:59:58 +0900

    Impl simple Server/Client

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace DummyClient
 {
@@ -6,7 +9,39 @@ namespace DummyClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 휴대폰 설정.
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+                // 문지기에게 입장문의.
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected to {socket.RemoteEndPoint}");
+
+                // 보낸다.
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                socket.Send(sendBuff);
+
+                // 받는다.
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                // 내보낸다.
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerCore
 {
@@ -6,7 +9,50 @@ namespace ServerCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 문지기.
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 문지기 교육.
+                listenSocket.Bind(endPoint);
+
+                // 영업시작.
+                // backlog: 최대 대기수.
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening ....");
+
+                    // 손님입장.
+                    Socket client = listenSocket.Accept(); // blocking
+
+                    // 받는다.
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = client.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // 보낸다.
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+                    client.Send(sendBuff);          // blocking.
+
+                    // 내보낸다.
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e}");
+            }
         }
     }
 }
