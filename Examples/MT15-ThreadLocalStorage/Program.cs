d80040e2773a1351d8f328b34b91769fa536a782
KihwanCheon <gujjy97@gmail.com>
2023-06-12 10:10:28 +0900

    Add TLS example

---
+++ Examples/MT15-ThreadLocalStorage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MT15_ThreadLocalStorage
+{
+    internal class Program
+    {
+        // private static string ThreadName;
+        private static ThreadLocal<string> ThreadName = new ThreadLocal<string>();
+        
+        static void WhoAmI()
+        {
+            // ThreadName = $"My Name is {Thread.CurrentThread.ManagedThreadId}";
+            ThreadName.Value = $"My Name is {Thread.CurrentThread.ManagedThreadId}";
+
+
+            Thread.Sleep(1000);
+            // Console.WriteLine(ThreadName);
+            Console.WriteLine(ThreadName.Value);
+        }
+
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+        }
+    }
+}
