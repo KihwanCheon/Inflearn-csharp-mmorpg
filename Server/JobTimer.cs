c4a5d646272b532c736b2d0df29677b0920f981e
KihwanCheon <gujjy97@gmail.com>
2023-09-14 07:47:50 +0900

    [Server] Impl JobTimer

---
+++ Server/JobTimer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using ServerCore;
+
+namespace Server
+{
+
+    struct JobTimerElement : IComparable<JobTimerElement>
+    {
+        public int ExecTick;// 실행 시각.
+        public Action Action;
+
+        public int CompareTo(JobTimerElement other)
+        {
+            return other.ExecTick - ExecTick;
+        }
+    }
+    public class JobTimer
+    {
+        PriorityQueue<JobTimerElement> _pq = new PriorityQueue<JobTimerElement>();
+        object _lock = new object();
+
+        public static JobTimer Instance { get; } = new JobTimer();
+
+        public void Push(Action action, int tickAfter = 0)
+        {
+            JobTimerElement job;
+            job.ExecTick = Environment.TickCount + tickAfter;
+            job.Action = action;
+
+            lock (_lock)
+            {
+                _pq.Push(job);
+            }
+        }
+
+        public void Flush()
+        {
+            while (true)
+            {
+                int now = Environment.TickCount;
+                JobTimerElement job;
+                lock (_lock)
+                {
+                    if (_pq.Count == 0)
+                        break;
+
+                    job = _pq.Peek();
+                    if (job.ExecTick > now)
+                        break;
+
+                    _pq.Pop();
+                }
+
+                job.Action.Invoke();
+            }
+        }
+    }
+}
