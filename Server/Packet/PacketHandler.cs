10af70578947ceb0d1594438509c29c971feb033
KihwanCheon <gujjy97@gmail.com>
2023-09-17 16:55:10 +0900

    [Server] Log player movements

--- Server/Packet/PacketHandler.cs
+++ Server/Packet/PacketHandler.cs
@@ -1,28 +1,33 @@
-﻿using Server;
+﻿using System;
+using Server;
 using ServerCore;
 
 
 public class PacketHandler
 {
-    public static void C_LeaveGameHandler(PacketSession session, IPacket packet)
+    public static void C_LeaveGameHandler(PacketSession pSession, IPacket ipkt)
     {
-        var clientSession = session as ClientSession;
-        if (clientSession?.Room == null)
+        var session = pSession as ClientSession;
+        if (session?.Room == null)
             return;
         
-        var room = clientSession.Room;
-        room.Push(() => room.Leave(clientSession));
+        var room = session.Room;
+        room.Push(() => room.Leave(session));
     }
 
-    public static void C_MoveHandler(PacketSession session, IPacket packet)
+    public static void C_MoveHandler(PacketSession pSession, IPacket ipkt)
     {
-        var clientSession = session as ClientSession;
-        if (clientSession?.Room == null)
+        var session = pSession as ClientSession;
+        if (session?.Room == null)
             return;
 
-        var movePkt = packet as C_Move;
-        var room = clientSession.Room;
+        var movePkt = ipkt as C_Move;
+        if (movePkt == null)
+            return;
 
-        room.Push( () => room.Move(clientSession, movePkt));
+        Console.WriteLine($"player {session.SessionId} move ({movePkt.posX}, {movePkt.posY}, {movePkt.posZ})");
+
+        var room = session.Room;
+        room.Push( () => room.Move(session, movePkt));
     }
 }
