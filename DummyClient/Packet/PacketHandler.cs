59f16137b10a57da008369f2fad4b9cce0bc7d2c
KihwanCheon <gujjy97@gmail.com>
2023-09-17 17:03:51 +0900

    [DummyClient] Log player enter/leave...

--- DummyClient/Packet/PacketHandler.cs
+++ DummyClient/Packet/PacketHandler.cs
@@ -6,25 +6,34 @@ public class PacketHandler
 {
     public static void S_BroadcastEnterGameHandler(PacketSession pSession, IPacket ipkt)
     {
-        var pkt = ipkt as S_BroadcastEnterGame;
         var session = pSession as ServerSession;
+
+        if (ipkt is S_BroadcastEnterGame pkt)
+            Console.WriteLine($"player {pkt.playerId} entered");
     }
 
     public static void S_BroadcastLeaveGameHandler(PacketSession pSession, IPacket ipkt)
     {
-        var pkt = ipkt as S_BroadcastLeaveGame;
         var session = pSession as ServerSession;
+
+        if (ipkt is S_BroadcastLeaveGame pkt)
+            Console.WriteLine($"player {pkt.playerId} left");
     }
 
     public static void S_PlayerListHandler(PacketSession pSession, IPacket ipkt)
     {
-        var pkt = ipkt as S_PlayerList;
         var session = pSession as ServerSession;
+
+        if (ipkt is S_PlayerList pkt)
+            Console.WriteLine($"{pkt.player.Count} players exist");
     }
 
     public static void S_BroadcastMoveHandler(PacketSession pSession, IPacket ipkt)
     {
-        var pkt = ipkt as S_BroadcastMove;
         var session = pSession as ServerSession;
+        var pkt = ipkt as S_BroadcastMove;
+        // if (pkt != null)
+        //     Console.WriteLine($"player {pkt.playerId} move ({pkt.posX}, {pkt.posY}, {pkt.posZ})");// if (pkt != null)
+        //     Console.WriteLine($"player {pkt.playerId} move ({pkt.posX}, {pkt.posY}, {pkt.posZ})");
     }
 }
