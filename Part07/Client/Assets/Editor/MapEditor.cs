be0c4f61e3b8ce1a1c1fac6333ae07dc9859b93e
KihwanCheon <gujjy97@gmail.com>
2023-09-27 06:25:53 +0900

    [Part07] Example - Add menu of custom tool

---
+++ Part07/Client/Assets/Editor/MapEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MapEditor : MonoBehaviour
+{
+#if UNITY_EDITOR
+
+    // short cut: % - Ctrl, # - Shift, & - Alt
+    [MenuItem("Tools/GenerateMap %#g")]
+    private static void HelloWorld()
+    {
+        if (EditorUtility.DisplayDialog("Hello World", "Create?", "Create", "Cancel"))
+        {
+            new GameObject("Hello World");
+        }
+    }
+#endif
+}
