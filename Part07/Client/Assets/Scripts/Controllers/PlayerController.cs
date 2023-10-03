e9f73ece85aaaa416dbd697afe037c91d663c0a5
KihwanCheon <gujjy97@gmail.com>
2023-10-04 06:57:45 +0900

    [Part07] Move simply 4 ways

---
+++ Part07/Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float _speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * Time.deltaTime * _speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * Time.deltaTime * _speed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
+        else
+        {
+        }
+    }
+}
